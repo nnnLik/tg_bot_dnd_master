16f30131cb8587cfb968e55a4ab4946003aadaa2
nnnLik <makhmudov_ra@vk.com>
2022-08-25 18:49:04 +0300

    add config

---
+++ config.py
@@ -0,0 +1,2 @@
+# Token of ur bot
+Token = ''
--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-# Объект бота
+# Bot object
 bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
-# Диспетчер для бота
 dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
 
-# Хэндлер на команду /test1
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
     await message.reply("Test 1")
 
 
+
 if __name__ == "__main__":
-    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
