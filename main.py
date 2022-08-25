ad7fb54c00c339b513c768e504c6753dc461ffcf
nnnLik <makhmudov_ra@vk.com>
2022-08-25 18:57:15 +0300

    add basicks handler

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
+import config
+
 # Bot object
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+bot = Bot(token=config.Token)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
 
-@dp.message_handler(commands="test1")
+@dp.message_handler(commands="start")
 async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+    await message.answer("Test")
 
-
+@dp.message_handler(commands="dice")
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
