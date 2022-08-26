104958799d4cca977322f255f01d584af037bbbf
nnnLik <makhmudov_ra@vk.com>
2022-08-26 23:07:53 +0300

    feat rebase log func from main

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+import json
+
 # Token of ur bot
 Token = '5756605309:AAG7t8Rluc6Vio_n3Ybk367CBeS4vJ-hrr0'
 # Get random hero list
 dir_hero_lists = 'hero_list/'
+# log to file
+def log(id, name, text):
+    with open('logs/log.json', mode='w') as log_file:
+        json.dump({
+        'id' : id,
+        'name' : name,
+        'text' : text
+        },
+        log_file)
