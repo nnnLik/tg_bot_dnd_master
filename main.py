000824ce0a9aea914e4ff0a98700b3c0b2d192b7
nnnLik <makhmudov_ra@vk.com>
2022-08-26 13:56:25 +0300

    feat all handbook tabs

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ Hi
 '''
 
 
+# Main menu
 @Dispatcher_bot.message_handler(commands="start")
 async def main_menu(message: types.Message):
 
@@ -34,6 +35,8 @@ async def main_menu(message: types.Message):
 
     await message.answer(Greating, reply_markup=keyboard)
 
+
+# HandBook
 @Dispatcher_bot.message_handler(lambda message: message.text == "Handbook")
 async def handbook(message: types.Message):
 
@@ -47,9 +50,85 @@ async def handbook(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(*buttons)
 
-    await message.answer("Here you can find all the <b>basic information</b>, which has been divided into parts for your convenience.", reply_markup=keyboard)
+    await message.answer("Here you can find all the <b>basic information</b>, which has been divided into parts for your convenience.",
+                         reply_markup=keyboard)
 
+# HandBook -> Movement/Action
+@Dispatcher_bot.message_handler(lambda message: message.text == "Movement/Action")
+async def handbook(message: types.Message):
 
+    buttons = ["Movement",
+               "Action",
+               "Bonus Action",
+               "Reactions",
+               "Free actions",
+               "Back to Menu"]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+
+    await message.reply("Choose the option that interests you",
+                         reply_markup=keyboard)
+
+# HandBook -> State
+@Dispatcher_bot.message_handler(lambda message: message.text == "State")
+async def handbook(message: types.Message):
+
+    buttons = ["State",
+               "Environment and Perception",
+               "Hits, Death, and Rest",
+               "Back to Menu"]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+
+    await message.reply("Choose the option that interests you",
+                         reply_markup=keyboard)
+
+# HandBook -> Damage
+@Dispatcher_bot.message_handler(lambda message: message.text == "Damage")
+async def handbook(message: types.Message):
+
+    buttons = ["Damage and Attacks",
+               "Spells",
+               "Back to Menu"]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+
+    await message.reply("Choose the option that interests you",
+                         reply_markup=keyboard)
+
+# HandBook -> Weapon
+@Dispatcher_bot.message_handler(lambda message: message.text == "Weapon")
+async def handbook(message: types.Message):
+
+    buttons = ["Weapons",
+               "Equipment sets",
+               "Tool",
+               "Back to Menu"]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+
+    await message.reply("Choose the option that interests you",
+                         reply_markup=keyboard)
+
+# HandBook -> Other
+@Dispatcher_bot.message_handler(lambda message: message.text == "Other")
+async def handbook(message: types.Message):
+
+    buttons = ["Travel",
+               "Multiclassing",
+               "Experience",
+               "Languages",
+               "Back to Menu"]
+
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+
+    await message.reply("Choose the option that interests you",
+                         reply_markup=keyboard)
 
 # Return to main menu
 @Dispatcher_bot.message_handler(lambda message: message.text == "Back to Menu")
@@ -65,12 +144,6 @@ async def back_menu(message: types.Message):
     keyboard.add(*buttons)
 
     await message.answer('<b>Whoo</b>', reply_markup=keyboard)
-
-
-# @dp.message_handler(commands="dice")
-# async def cmd_dice(message: types.Message):
-#     await message.answer_dice(emoji="🎲")
-
 
 if __name__ == "__main__":
     executor.start_polling(Dispatcher_bot, skip_updates=True)
