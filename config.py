1cbc7d1c569de2c29b282e075551af67c9599453
nnnLik <makhmudov_ra@vk.com>
2022-08-27 10:13:30 +0300

    fix read func

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ Token = '5756605309:AAG7t8Rluc6Vio_n3Ybk367CBeS4vJ-hrr0'
 
 # Get random hero list
 dir_hero_lists = 'hero_list/'
+logs = 'logs/log.json'
+faces = 'logs/faces.json'
 
 # log to file
 def log(id, name, text):
-    with open('logs/log.json', mode='w') as log_file:
+    with open(logs, mode='w') as log_file:
         json.dump({
         'id' : id,
         'name' : name,
@@ -17,6 +19,11 @@ def log(id, name, text):
         log_file)
 
 # log faces
-def wr_faces(faces):
-    with open('logs/faces.json',mode='w') as fac:
-        json.dump({'faces' : faces}, fac)
+def wr_faces(fac_of_dice):
+    with open(faces, mode='w') as fac:
+        json.dump({'faces' : fac_of_dice}, fac)
+
+def rd_faces():
+    with open(faces, mode='r', encoding='utf-8') as fac:
+        text = json.load(fac)
+        return text['faces']
