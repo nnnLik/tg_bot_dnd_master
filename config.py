3d242c9d69c7f1fcccfc63cbf184444f27352a02
nnnLik <makhmudov_ra@vk.com>
2022-08-27 10:02:38 +0300

    feat fin for write dises face

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ import json
 
 # Token of ur bot
 Token = '5756605309:AAG7t8Rluc6Vio_n3Ybk367CBeS4vJ-hrr0'
+
 # Get random hero list
 dir_hero_lists = 'hero_list/'
+
 # log to file
 def log(id, name, text):
     with open('logs/log.json', mode='w') as log_file:
@@ -13,3 +15,8 @@ def log(id, name, text):
         'text' : text
         },
         log_file)
+
+# log faces
+def wr_faces(faces):
+    with open('logs/faces.json',mode='w') as fac:
+        json.dump({'faces' : faces}, fac)
